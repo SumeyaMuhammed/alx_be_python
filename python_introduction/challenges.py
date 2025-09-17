9a0eeb5fa1754fbe75e1baff603b1a89a4ba72b3
Sumeya Muhammed <sumenaweya@gmail.com>
2025-09-16 17:15:15 -1100

    Adding numbers using for loop

--- python_introduction/challenges.py
+++ python_introduction/challenges.py
@@ -1,20 +1,5 @@
-import random
-
-secret_number = random.randint(1, 10)
-guess = int(input("I'm thinking of a number between 1 and 10. Can you guess it?"))
-
-match guess:
-  case x if x ==secret_number:
-    print("Congratulations, you guessed it!")
-  case _:
-        if guess > secret_number:
-          print("Nope, your guess is a bit high.")
-        elif guess < secret_number:
-          print("Nope, your guess is a bit low.")
-        guess = int(input( "Give it another shot!"))
-
-        match guess:
-          case x if  x == secret_number:
-            print("Congratulations, you guessed it!")
-          case _:
-              print("Nope, Your guess is incorrect. Try again later.")
+numbers = [1, 2, 3, 4, 5]
+total = 0
+for num in numbers:
+  total += num
+print(total)
