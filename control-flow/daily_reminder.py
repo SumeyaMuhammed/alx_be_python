58d3eac69cba58340f059bb24007c150f5169229
Sumeya Muhammed <sumenaweya@gmail.com>
2025-09-20 06:16:06 -1100

    add reminder and note

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,13 +4,13 @@ priority = input("Priority (high/medium/low): ")
 
 match priority:
   case "high":
-    print(f"'{task}' is a high priority task", end="")
+    print(f"'Reminder: {task}' is a high priority task", end="")
     if time_bound == "yes":
       print(" that requires immediate attention today!")
     else:
       print(". Consider completing it when you have free time.")
   case "low":
-    print(f"'{task}' is a low priority task", end="")
+    print(f"Note: '{task}' is a low priority task", end="")
     if time_bound == "yes":
       print(" that requires immediate attention today!")
     else:
