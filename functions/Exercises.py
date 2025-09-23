ca39426685167af126b8fb34bac944e19b82672d
Sumeya Muhammed <sumenaweya@gmail.com>
2025-09-22 18:34:14 -1100

    Data Structures Exercise

--- functions/Exercises.py
+++ functions/Exercises.py
@@ -1,6 +1,24 @@
-def even_odd(num):
-  reminder = num%2
-  if reminder == 0:
-    print(f"{num} is an even number")
-  else:
-    print(f"{num} is an odd number")
+#Practice Exercise
+
+"""Exercise 1: Create a list to store names of your favorite fruits. Write code to access the second element and print it."""
+
+Favorite_Fruits = ["apple", "orange", "watermelon"]
+print(Favorite_Fruits[1])
+
+"""Exercise 2: Define a dictionary to store information about your favorite book, including title, author, and genre. Use the method to retrieve the genre."""
+
+Favorite_Book = {"Title": "Atomic Habits", "Author": "James Clear", "Genre": "Self-help"}
+
+print(Favorite_Book["Genre"])
+
+"""Exercise 3: Write a program to generate a random set of numbers between 1 and ten. Use set operations to remove duplicates and display the unique numbers.
+"""
+
+import random
+numbers = []
+for i in range(10):
+  numbers.append(random.randint(1, 10))
+
+unique_set = set(numbers)
+print(unique_set)
+
