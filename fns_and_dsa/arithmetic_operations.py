b213c489b659b18028a835a3195126a073d6aa9a
Sumeya Muhammed <sumenaweya@gmail.com>
2025-09-22 19:13:10 -1100

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
     case "multiply":
       result = num1 * num2
     case "divide":
-      if num2 ==0:
+      if num2 == 0:
         return "Cannot divide by zero"
       else:
         result = num1 / num2
