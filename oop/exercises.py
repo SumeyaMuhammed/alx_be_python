4841099e1082135d0e29c359dc4396c9f8769ce5
Sumeya Muhammed <sumenaweya@gmail.com>
2025-10-10 07:51:41 -1100

    More on Class Methods and Static Methods

--- oop/exercises.py
+++ oop/exercises.py
@@ -1,53 +1,36 @@
-# Exercise 1: Single Inheritance Instruction:
-class Shape:
-  def calculate_area(self, area):
-    return area
+# Exercise 1: Class Methods for Counting Instances Instruction:
+
+class Book:
+
+  total_books = 0
+  def __init__(self, title):
+    self.new_book = title
+    Book.total_books += 1
+  @classmethod
+  def display_total_books(cls):
+    print(f"Total books created: {cls.total_books}")
+
+
+# Exercise 2: Static Method for Utility Calculation Instruction:
+
+class Calculator:
+
+  @staticmethod
+  def add(num1, num2):
+    return num1 + num2
   
-class Rectangle(Shape):
-  def __init__(self, len, wid):
-    self.length = len
-    self.width = wid
-  def calculate_area(self):
-    return self.length * self.width
-
-
-# Exercise 2: Multiple Inheritance Instruction:
-
-class Bird:
-  def fly(self):
-    return "It flies!"
+  @staticmethod
+  def multiply(num1, num2):
+    return num1 * num2
   
-class Mammal:
-  def run(self):
-    return "It runs!"
+# Exercise 3: Class Method for Factory Creation Instruction:
 
-class Bat(Bird, Mammal):
-  def fly(self):
-    return "It flies!"
-  
-  def run(self):
-    return "It sometimes runs!"
+class Person:
 
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-# Exercise 3: Polymorphism with Duck Typing Instruction:
-
-class Dog:
-  def make_sound(self):
-    return "Woof!"
-  
-class Cat:
-  def make_sound(self):
-    return "Meow!"
-  
-class Bird:
-  def make_sound(self):
-    return "Chirp!"
-  
-def let_them_speak(animals):
-  for animal in animals:
-    print(animal.make_sound())
-
-sample_animals = [Cat(), Dog(), Bird()]    
-
-let_them_speak(sample_animals)
-
+  @classmethod
+  def create_child(cls, name = "new_child"):
+    return cls(name, 0)
